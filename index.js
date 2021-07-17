1ce5be4b00237457fdb5ba26d0a96739f4c0e8b4
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-07-17 06:10:04 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function myfun(Number){
+    if(Number>0){
+        console.log("the number is positive")
+    }
+    else
+    {
+        console.log("the number is negative")
+    }
+     
+    
+}
+myfun(-5)
+
+function swi(num){
+    switch(num>0){
+        case a:
+            "the number is  positive"
+            break;
+        default:
+            "the number is negative"    
+    }
+}
+swi(5)
